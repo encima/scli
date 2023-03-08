bdb5e3f7b7c7486553be9bf0cd53dfd944cd1377
Chris Gwilliams <encima@gmail.com>
2023-03-08 13:24:51 +0100

    migrate to pipenv and handle recording history of what was read

--- scli.py
+++ scli.py
@@ -1,4 +1,6 @@
-import time, sys, argparse
+import time
+import sys
+import argparse
 from subprocess import call
 from blessed import Terminal
 from newspaper import Article
@@ -6,11 +8,12 @@ from datetime import datetime
 import csv
 import os
 
+
 def read_file(f_name):
     words = []
     with open(f_name) as f:
-	    for line in f:
-		    words += line.split(' ')
+        for line in f:
+            words += line.split(' ')
     return words
 
 
@@ -21,13 +24,15 @@ def read_link(address):
     page.parse()
     return page.text.split(' ')
 
+
 def speed_read(words, speed):
     term = Terminal()
     call(['clear'])
     with term.fullscreen():
         for w in words:
             printable = w
-            print(term.move_y(term.height // 2) + term.center(term.bold(printable)).rstrip())
+            print(term.move_y(term.height // 2) +
+                  term.center(term.bold(printable)).rstrip())
             time.sleep(speed)
             call(["clear"])
 
@@ -35,14 +40,16 @@ def speed_read(words, speed):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Speed read!')
     parser.add_argument('-l', action='store', dest='link_address',
-                    help='Use a link as the source')
+                        help='Use a link as the source')
     parser.add_argument('-f', action='store', dest='f_name',
-                    help='Use a file as the source')
+                        help='Use a file as the source')
     parser.add_argument('-p', action='store', dest='plain',
-                    help='Use provided text as the source (surround with quotes)')
-    parser.add_argument('-d', action='store', dest='db', help='File to store read articles', default='/home/encima/vimwiki/scli.csv')
+                        help='Use provided text as the source (surround with quotes)')
+    parser.add_argument('-S', '--save', type=bool, default=False)
+    parser.add_argument('-d', action='store', dest='db',
+                        help='File to store read articles', default=os.path.expandvars('$HOME/scli.csv'))
     parser.add_argument('-s', action='store', dest='speed', default=0.2,
-                    help='Set the speed', type=float)
+                        help='Set the speed', type=float)
 
     args = parser.parse_args()
     text = ""
@@ -57,10 +64,11 @@ if __name__ == '__main__':
 
     if len(text) > 0:
         speed_read(text, args.speed)
-        mode = 'a'
-        if not os.path.exists(args.db):
-            mode = 'w+'
-        fd = open(args.db, mode)
-        r = args.link_address if args.link_address else args.f_name
-        fd.write('{}, {}\n'.format(r, datetime.today()))
-        fd.close()
+        if args.save:
+            mode = 'a'
+            if not os.path.exists(args.db):
+                mode = 'w+'
+            fd = open(args.db, mode)
+            r = args.link_address if args.link_address else args.f_name
+            fd.write('{}, {}\n'.format(r, datetime.today()))
+            fd.close()
